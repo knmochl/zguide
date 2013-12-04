da6691e54bb551dbc06da06967560925a9a21ccc
Nathan Sullivan <nsullivan@ariba.com>
2013-12-03 17:47:11 -0800

    Changes to client task

--- examples/Haskell/peering3.hs
+++ examples/Haskell/peering3.hs
@@ -13,14 +13,14 @@ import System.ZMQ3.Monadic
 type Worker = String
 type Broker = String
 
-data SocketGroup z a = SocketGroup {
-      localFE :: Socket z a
-    , localBE :: Socket z a
-    , cloudFE :: Socket z a
-    , cloudBE :: Socket z a
-    , stateFE :: Socket z a
-    , stateBE :: Socket z a
-    , monitor :: Socket z a
+data SocketGroup z router sub pub pull = SocketGroup {
+      localFE :: Socket z router
+    , localBE :: Socket z router
+    , cloudFE :: Socket z router
+    , cloudBE :: Socket z router
+    , stateFE :: Socket z sub
+    , stateBE :: Socket z pub
+    , mon     :: Socket z pull
 }
 
 workerReady :: String
@@ -49,27 +49,33 @@ unwrapMessage msg = let msg' = tail msg
 sendToWorker :: (Sender a) => Socket z a -> Worker -> [String] -> ZMQ z ()
 sendToWorker sock worker msg = sendMulti sock $ fromList $ map pack $ worker : "" : msg
 
-clientTask :: Broker -> ZMQ z ()
-clientTask broker = do
+startClientTask :: Broker -> ZMQ z ()
+startClientTask broker = do
     client <- socket Req
     connect client $ "ipc://" ++ broker ++ "-localfe.ipc"
     monitor <- socket Push
     connect monitor $ "ipc://" ++ broker ++ "-monitor.ipc"
+    clientTask client monitor
 
-    forever $ do
-        sleepTime <- liftIO $ getRandomInt (1,5)
-        messageCount <- liftIO $ getRandomInt (1,15)
-        liftIO $ threadDelay $ sleepTime * 1000
-        replicateM_ messageCount $ do
-            randomTask <- liftIO $ getRandomInt (0, 16 * 16 * 16 * 16)
-            let taskId = printf "%04X" randomTask
-            send client [] $ pack taskId
-            [evts] <- poll (10 * 1000) [Sock client [In] Nothing]
-            if In `elem` evts then do
-                reply <- receive client
-                send client [] reply
+clientTask :: (Receiver a, Sender a, Sender b) => Socket z a -> Socket z b -> ZMQ z ()
+clientTask client monitor = do
+    sleepTime <- liftIO $ getRandomInt (1,5)
+    messageCount <- liftIO $ getRandomInt (1,15)
+    liftIO $ threadDelay $ sleepTime * 1000
+    replicateM_ messageCount $ do
+        randomTask <- liftIO $ getRandomInt (0, 16 * 16 * 16 * 16)
+        let taskId = printf "%04X" randomTask
+        send client [] $ pack taskId
+        [evts] <- poll (10 * 1000) [Sock client [In] Nothing]
+        if In `elem` evts then do
+            msg <- receiveMessage client
+            if taskId == (head msg) then do
+                send monitor [] $ pack $ head msg
             else do
-                send monitor [] $ pack $ "E: CLIENT EXIT - lost task " ++ taskId
+                send monitor [] $ pack $ "Wrong task: " ++ (head msg)
+        else do
+            send monitor [] $ pack $ "Lost task: " ++ taskId
+    clientTask client monitor
 
 workerTask :: Broker -> ZMQ z ()
 workerTask broker = do
@@ -83,13 +89,14 @@ workerTask broker = do
         liftIO $ threadDelay $ sleepTime * 1000
         sendToWorker worker target msg'
 
-routeTraffic :: (Receiver a, Sender a) => [Broker] -> [Worker] -> SocketGroup z a -> ZMQ z ()
+routeTraffic :: [Broker] -> [Worker] -> SocketGroup z router sub pub pull -> ZMQ z ()
 routeTraffic peers workers sockets = do
     let timeout = if workers == [] then (-1) else 1000
-    poll timeout [Sock (localBE sockets) [In] Nothing,
-                  Sock (cloudBE sockets) [In] Nothing,
-                  Sock (stateFE sockets) [In] Nothing,
-                  Sock (monitor sockets) [In] Nothing]
+    evts <- poll timeout [Sock (localBE sockets) [In] Nothing,
+                          Sock (cloudBE sockets) [In] Nothing,
+                          Sock (stateFE sockets) [In] Nothing,
+                          Sock (mon sockets) [In] Nothing]
+    return ()
 
 main = do
     args <- getArgs
@@ -122,6 +129,6 @@ main = do
         monitor <- socket Pull
         bind monitor $ "ipc://" ++ me ++ "-monitor.ipc"
         let sockets = SocketGroup localFront localBack cloudFront cloudBack stateFront stateBack monitor
-        replicateM_ numWorkers (async $ clientTask me)
-        replicateM_ numClients (async $ workerTask me)
+        replicateM_ numWorkers (async $ workerTask me)
+        replicateM_ numClients (async $ startClientTask me)
         --routeTraffic peers [] sockets
